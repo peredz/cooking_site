2703d5d0524e029ca29e64ef0d2ac37a57c3764c
peredz <94736048+peredz@users.noreply.github.com>
2022-04-21 19:27:09 +1000

    only header with nothing

--- auto_correction.py
+++ auto_correction.py
@@ -4,7 +4,7 @@ import os
 params = {
   "api_key": "5ead802eb4382388831a3c1daac78d3cf9b8e16312638ddd1dd64723a6c1247d",
   "engine": "google",
-  "q": "штрудел",
+  "q": "штрудиль",
   "gl": "ru",
   "hl": "ru"
 }
